43b041a9f05636fde1df38e942e8a92869f04523
Михаил Калинин <benzol.45@gmail.com>
2023-02-21 12:37:47 +0100

    Add Logback logging

---
+++ src/main/java/LoggerLesson.java
@@ -0,0 +1,21 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+
+
+public class LoggerLesson {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerLesson.class);
+
+    public static void main(String[] args) {
+        while (true) {
+            logger.info("Message for logging at time: {}", LocalDateTime.now());
+
+            try {
+                Thread.sleep(1 * 1000);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+}
